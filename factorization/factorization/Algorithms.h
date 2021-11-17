631772b3946f97c35a62c7539aedacf2a926204f
saniatok <saniat.ok@mail.ru>
2021-11-17 19:57:40 +0300

    run, detecting time

---
+++ factorization/factorization/Algorithms.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <vector>
+template <typename T>
+using Algorithms = std::vector<T>(*)(std::vector<T>);
+
---
+++ factorization/factorization/factorization.cpp
@@ -0,0 +1,25 @@
+﻿#include <vector>
+#include <chrono>
+#include <algorithm>
+#include <string>
+#include <iostream>
+#include "Algorithms.h"
+using namespace std;
+
+template <typename T>
+void Run(string method_name, Algorithms<T> p, vector<T> data)
+{
+    auto start = chrono::system_clock::now();
+    vector<T> res = p(data);
+    auto stop = chrono::system_clock::now();
+    auto time = chrono::duration_cast<chrono::microseconds>(stop - start).count();
+    cout << method_name << "\t"
+        << data.size() << "\t"
+        << (is_sorted(res.begin(), res.end()) ? to_string(time) + "\tmcs" : "failed") << endl;
+}
+
+int main()
+{
+   
+    
+}
