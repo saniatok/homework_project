7fdebfcf9a4f3b5d580c0cf1589db673cc80f7a5
Nikitin Alexander <40249422+Bestyara@users.noreply.github.com>
2021-11-24 09:19:23 +0300

    Update Algorithms.h
    Added Pollard's method p-1

--- factorization/factorization/Algorithms.h
+++ factorization/factorization/Algorithms.h
@@ -8,5 +8,6 @@ using Algorithms = vector<int>(*)(T data);
 
 vector<int> fraction_method(int data);
 vector<int> p_method_Pollard(int data);
+vector<int> metodpollardap1(int data);
 vector<int>FermatFactor(int data);
 vector<int> pethod_Leman(int data);
